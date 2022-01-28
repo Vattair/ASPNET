29ac04123172d0151709434c4ca4bb54d0afe9bf
Kamil Karliński <kamil.karlinski@microsoft.wsei.edu.pl>
2022-01-28 18:59:37 +0100

    migration

---
+++ ASPNET/Migrations/20220128175851_newmig1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ASPNET.Migrations
+{
+    public partial class newmig1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
